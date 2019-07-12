12a8c2096c638b34c1885a5db5aba2dbfac62940
Toh Wen Xiang <tohwenxiang@gmail.com>
2019-07-12 13:21:38 +0800

    Started on implementing marching squares.

---
+++ Assets/Scripts/ControlNode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlNode : Node
+{
+    public bool active;
+    public Node aboveNode;
+    public Node rightNode;
+
+    public ControlNode(Vector3 pos, bool _active, float squareSize) : base(pos)
+    {
+        active = _active;
+        aboveNode = new Node(pos + (Vector3.forward * (squareSize * 0.5f)));
+        rightNode = new Node(pos + (Vector3.right * (squareSize * 0.5f)));
+    }
+}
---
+++ Assets/Scripts/Square.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour
+{
+    public ControlNode topLeft;
+    public ControlNode topRight;
+    public ControlNode bottomLeft;
+    public ControlNode bottomRight;
+
+    public Node centerTop;
+    public Node centerRight;
+    public Node centerBottom;
+    public Node centerLeft;
+
+    public Square(ControlNode _topLeft, ControlNode _topRight, ControlNode _bottomRight, ControlNode _bottomLeft)
+    {
+
+    }
+}
