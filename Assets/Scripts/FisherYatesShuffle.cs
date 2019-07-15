6547f6b405174b653c829f7b48109f13e35d9d14
Toh Wen Xiang <tohwenxiang@gmail.com>
2019-07-15 14:12:10 +0800

    Implementing FisherYates Shuffle...

---
+++ Assets/Scripts/FisherYatesShuffle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Shuffle
+{
+    //return a shuffled array of cell index
+    static Vector2Int[,] FisherYatesShuffle(Vector2Int[,] unOccupiedCellIndex, System.Random randNumGen)
+    {
+        Vector2Int[,] shuffledUnOccupiedCellIndex;
+        //start from the last item and moving towards the start
+        for (int y = unOccupiedCellIndex.GetLength(1) - 1; y > 0; y--)
+        {
+            for (int x = unOccupiedCellIndex.GetLength(0) - 1; x > 0; x--)
+            {
+                //get a random cell index
+                Vector2Int randomCellIndex = new Vector2Int(randNumGen.Next(0, x + 1), randNumGen.Next(0, y + 1));
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public Vector3 position;
+    public int vertexIndex = -1;
+    
+    public Node(Vector3 pos)
+    {
+        position = pos;
+    }
+}
