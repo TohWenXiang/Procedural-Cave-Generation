e7e633e2675d86aadd52c12f940c9a47947be5b3
Toh Wen Xiang <tohwenxiang@gmail.com>
2019-07-22 14:25:49 +0800

    Implemented Fisher Yates Shuffle. Up next, applying and testing.

--- Assets/Scripts/FisherYatesShuffle.cs
+++ Assets/Scripts/FisherYatesShuffle.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public static class Shuffle
 {
     //return a shuffled array of cell index
-    static Vector2Int[,] FisherYatesShuffle(Vector2Int[,] unOccupiedCellIndex, System.Random randNumGen)
+    public static List<Vector2Int> FisherYates(List<Vector2Int> input, System.Random randNumGen)
     {
-        Vector2Int[,] shuffledUnOccupiedCellIndex;
-        //start from the last item and moving towards the start
-        for (int y = unOccupiedCellIndex.GetLength(1) - 1; y > 0; y--)
+        for (int i = input.Count - 1; i > 0; i--)
         {
-            for (int x = unOccupiedCellIndex.GetLength(0) - 1; x > 0; x--)
-            {
-                //get a random cell index
-                Vector2Int randomCellIndex = new Vector2Int(randNumGen.Next(0, x + 1), randNumGen.Next(0, y + 1));
-            }
+            int randomIndex = randNumGen.Next(0, i);
+
+            Vector2Int temp = input[randomIndex];
+
+            input[randomIndex] = input[i];
+            input[i] = temp;
         }
+
+        return input;
     }
 }
 
